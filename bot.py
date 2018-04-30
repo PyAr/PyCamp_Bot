bdf4e5b9b1791a17465b8c13ddd10a81dc8e9f17
Arthur <arthuryanik@gmail.com>
2018-04-30 11:59:07 -0300

    /ayuda command updated to reflect current changes in functionality.

--- bot.py
+++ bot.py
@@ -90,9 +90,11 @@ def ayuda(bot, update):
         
         Primera etapa: Lxs responsables de los proyectos cargan sus proyectos mediante el comando /cargar_proyecto. Solo un responsable carga el proyecto, y luego si hay otrxs responsables adicionales, pueden agregarse con el comando /ownear.
         
-        Segunda etapa: Mediante el comando /votar todxs lxs participantes votan los proyectos que se expongan. Esto se puede hacer a medida que se expone, o al haber finalizado todas las exposiciones.
+        Segunda etapa: Mediante el comando /votar todxs lxs participantes votan los proyectos que se expongan. Esto se puede hacer a medida que se expone, o al haber finalizado todas las exposiciones. Si no se está segurx de un proyecto, conviene no votar nada, ya que luego podés volver a ejecutar el comando y votar aquellas cosas que no votaste. NO SE PUEDE CAMBIAR TU VOTO UNA VEZ HECHO.
         
-        Tercera etapa: Lxs admins mergean los proyectos que se haya decidido mergear durante las exposiciones (Por tematica similar, u otros motivos), y luego se procesan los datos para obtener el cronograma final.''')
+        Tercera etapa: Lxs admins mergean los proyectos que se haya decidido mergear durante las exposiciones (Por tematica similar, u otros motivos), y luego se procesan los datos para obtener el cronograma final.
+        
+        Comandos adicionales: /ser_magx te transforma en el/la Magx de turno. /evocar_magx pingea a la/el Magx de turno que necesitas su ayuda. Con un gran poder, viene una gran responsabilidad''')
 
 def load_project(bot, update):
     '''Command to start the cargar_proyectos dialog'''
