e2106a7e451884c75ce2a3e65a319aa1d7f5dea0
Arthur <arthuryanik@gmail.com>
2018-05-01 11:55:14 -0300

    Fixed /merge typo's. Documented /merge.

--- bot.py
+++ bot.py
@@ -177,6 +177,7 @@ def merge(bot, update):
     if update.message.from_user.username in autorizados:
         lista_proyectos = [p.name for p in Project.select()]
         dic_proyectos = dict(enumerate(lista_proyectos))
+        #Asks for user input regarding first project
         bot.send_message(
             chat_id = update.message.chat_id,
             text = "Decime el primer proyecto que querés combinar (En número)"
@@ -199,6 +200,7 @@ def merge(bot, update):
         )
 
 def primer_proyecto(bot, update):
+    #Grabs first response and asks for the second one
     username = update.message.from_user.username
     text = update.message.text
     chat_id = update.message.chat_id
@@ -209,47 +211,45 @@ def primer_proyecto(bot, update):
     bot.send_message(
         chat_id = update.message.chat_id,
         text = "Decime el segundo proyecto que querés combinar (En número)"
-    )
-    for k,v in dic_proyectos.items():
-        bot.send_message(
-            chat_id = update.message.chat_id,
-            text = "{}: {}".format(k,v)
-
         )
-    bot.send_message(
-        chat_id = update.message.chat_id,
-        text = "------------------------------------------------------------------------------")
     return SEGUNDO_PROYECTO
 
 def segundo_proyecto(bot,update):
     username = update.message.from_user.username
     text = update.message.text
     chat_id = update.message.chat_id
+    #Grabs second reply
 
     lista_proyectos = [p.name for p in Project.select()]
     dic_proyectos = dict(enumerate(lista_proyectos))
     proyecto_secundario = dic_proyectos[int(text)]
     merge_projects.append(proyecto_secundario)
 
+    # Queries data for both projects to be merged
     proyecto_primario_query = Project.get(Project.name==merge_projects[0])
     proyecto_secundario_query = Project.get(Project.name==merge_projects[1])
 
+    # Queries for positive votes matching the project's ID
     query_votos_primario = Vote.select().where(Vote.project==proyecto_primario_query, Vote.interest==1)
     query_votos_secundario = Vote.select().where(Vote.project==proyecto_secundario_query, Vote.interest==1)
 
+    # Creates list with user IDs from previous votes, combines them and creates a list with non repeating items
     lista_votos_primario = [voto.pycampista_id for voto in query_votos_primario]
     lista_votos_secundario = [voto.pycampista_id for voto in query_votos_secundario]
     lista_unica = lista_votos_primario.copy()
-    lista_unica.extend(lista_2)
+    lista_unica.extend(lista_votos_secundario)
     lista_unica = set(lista_unica)
 
+    # Iterates through the unique list and any voter that is not present in the first project, gets its user ID loaded with a positive vote into the first project.
     for i in lista_unica:
         if i not in lista_votos_primario:
             Vote.get_or_create(project_id=proyecto_primario_query.id, pycampista_id=i,interest=1)
 
+    # Queries for project owner ID and project ID matching the first and second project query
     project_owner_id_primario = ProjectOwner.select().where(ProjectOwner.project_id==proyecto_primario_query)
     project_owner_id_secundario = ProjectOwner.select().where(ProjectOwner.project_id==proyecto_secundario_query)
 
+    # Creates list with user IDs from owners, combines them and creates a list with non repeating items
     lista_owner_primario = [proyecto.owner_id for proyecto in project_owner_id_primario]
     lista_owner_secundario = [proyecto.owner_id for proyecto in project_owner_id_secundario]
 
@@ -257,19 +257,32 @@ def segundo_proyecto(bot,update):
     lista_unica_owner.extend(lista_owner_secundario)
     lista_unica_owner = set(lista_unica_owner)
 
+    # Iterates through the unique list and any owner that is not present in the first project, gets its user ID loaded as an owner of the first project.    
     for i in lista_unica_owner:
         if i not in lista_owner_primario:
             ProjectOwner.get_or_create(project_id=proyecto_primario_query.id, owner_id=i)
 
+    # Combines both names into the first project name
     nombre_primario=proyecto_primario_query.name
     nombre_secundario=proyecto_secundario_query.name
     nuevo_nombre_proyecto=nombre_primario + " / " + nombre_secundario
     proyecto_primario_query.name = nuevo_nombre_proyecto
     proyecto_primario_query.save()
+
+    # Deletes second project data and allows the dev to insert Rage Against the Machine reference.
+    killing_in_the_name_of = Project.delete().where(Project.id==proyecto_secundario_query.id)
+    bulls_on_parade = Vote.delete().where(Vote.project_id==proyecto_secundario_query.id)
+    calm_like_a_bomb = ProjectOwner.delete().where(ProjectOwner.project_id==proyecto_secundario_query.id)
+
+    killing_in_the_name_of.execute()
+    bulls_on_parade.execute()
+    calm_like_a_bomb.execute()
+
     bot.send_message(
         chat_id=update.message.chat_id,
-        text= "Los proyectos ya se encuentran combinados."
+        text= "Los proyectos han sido combinados."
     )
+
 
 
 
