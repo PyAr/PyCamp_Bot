4b29a5447895086aee2b651fd4e565b4d8dbed01
WinnaZ <zoe@enzona.com>
2019-03-03 14:13:14 -0300

    fix bug no te deja startear si no tenes un username

--- bin/bot.py
+++ bin/bot.py
@@ -28,9 +28,20 @@ logger = logging.getLogger(__name__)
 def start(bot, update):
     logger.info('Start command')
     chat_id = update.message.chat_id
-    bot.send_message(
-        chat_id=chat_id,
-        text='Hola ' + update.message.from_user.first_name + '! Bienvenidx')
+    
+    if update.message.from_user.username is None:
+           bot.send_message(
+                chat_id=chat_id,
+                text="""Hola! Necesitas tener un username primero.
+                        \nCreate uno siguiendo esta guia: https://ewtnet.com/technology/how-to/how-to-add-a-username-on-telegram-android-app.
+                        Y despues dame /start the nuevo :) """)
+      
+
+    elif update.message.from_user.username:
+        bot.send_message(
+                chat_id=chat_id,
+                text='Hola ' + update.message.from_user.username + '! Bienvenidx')
+     
 
 
 def text_input(bot, update):
