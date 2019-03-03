7bf906212c8652d489b089a6a6b5d295daca9d43
cancerbero <xcancerberox@gmail.com>
2019-03-03 16:52:30 -0300

    Sacamos la utilizacion de wizard y agregamos un chequeo de existencia

--- src/pycamp_bot/wizard.py
+++ src/pycamp_bot/wizard.py
@@ -23,8 +23,15 @@ def become_wizard(bot, update):
 
 def summon_wizard(bot, update):
     username = update.message.from_user.username
-    wizard = Wizard.get(Wizard.current is True)
-    bot.send_message(
-        chat_id=wizard.chat_id,
-        text="PING PING PING MAGX! @{} te necesesita!".format(username)
-    )
+    try:
+        wizard = Pycampista.get(Pycampista.wizard is True)
+        bot.send_message(
+            chat_id=wizard.chat_id,
+            text="PING PING PING MAGX! @{} te necesesita!".format(username)
+        )
+    except Pycampista.DoesNotExist:
+        bot.send_message(
+            chat_id=update.chat_id,
+            text="Hubo un accidente, el mago esta en otro plano.".format(username)
+        )
+
