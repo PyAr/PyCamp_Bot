463791270081017771eef8bd16aa86608088363f
Zoe Marino <zoe@enzona.com>
2022-03-25 14:58:07 -0300

    fix bug: now you can /anunciar projects with n word names

--- src/pycamp_bot/commands/announcements.py
+++ src/pycamp_bot/commands/announcements.py
@@ -7,7 +7,9 @@ from pycamp_bot.models import Project, Pycampista, Vote
 
 logger = logging.getLogger(__name__)
 def announce(bot, update):
-    project_name = update.message.text.split()[1]
+    project_name = update.message.text.split()[1:]
+
+    project_name = " ".join(project_name)
     project =Project.select().where(Project.name ==project_name)
     if len(project) <= 0:
         bot.send_message(
@@ -18,15 +20,19 @@ def announce(bot, update):
         )
         return
 
-
     pycampistas = Vote.select().join(Pycampista).where(Vote.project == project.get())
 
     chat_id_list = [user.pycampista.chat_id for user in pycampistas]
-
+    chat_id_list = [53517488]
+    import ipdb; ipdb.set_trace()
     for chat_id in chat_id_list:
         bot.send_message(
             chat_id=chat_id,
-            text=f"Esta por empezar {project_name} a cargo de @{project.owner.username}"
+            text=f"Esta por empezar {project_name} a cargo de @{project.get().owner.username}."
+        )
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text=f"Anunciado!"
         )
 
 def set_handlers(updater):
