e4ee517cb77ef76f7d3c758bfbdde48cae1d22fe
Alexmary Romero <alexmarygr@gmail.com>
2023-03-25 15:48:24 -0300

    Fix summon wizard

--- src/pycamp_bot/commands/wizard.py
+++ src/pycamp_bot/commands/wizard.py
@@ -1,5 +1,6 @@
 from telegram.ext import CommandHandler
 from pycamp_bot.models import Pycampista
+import random
 
 
 async def become_wizard(update, context):
@@ -24,18 +25,22 @@ async def become_wizard(update, context):
 
 async def summon_wizard(update, context):
     username = update.message.from_user.username
-    try:
-        wizard = Pycampista.get(Pycampista.wizard is True)
+    wizard_list = list(Pycampista.select().where(Pycampista.wizard==True))
+    if len(wizard_list) == 0:
+        await context.bot.send_message(
+            chat_id=update.message.chat_id,
+            text="No hay ningunx magx todavia"
+        )
+    else:
+        wizard = random.choice(wizard_list)
         await context.bot.send_message(
             chat_id=wizard.chat_id,
             text="PING PING PING MAGX! @{} te necesesita!".format(username)
         )
-    except Pycampista.DoesNotExist:
         await context.bot.send_message(
-            chat_id=update.chat_id,
-            text="Hubo un accidente, el mago esta en otro plano.".format(username)
+            chat_id=update.message.chat_id,
+            text="Tu magx asignadx es: @{}".format(wizard.username)
         )
-
 
 def set_handlers(application):
     application.add_handler(
