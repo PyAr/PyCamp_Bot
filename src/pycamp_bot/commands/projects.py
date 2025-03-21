21de986c466b52ba7ad3160dae62b145860db83a
WinnaZ <zoe@enzona.com>
2025-03-21 16:24:59 -0300

    added punto

--- src/pycamp_bot/commands/projects.py
+++ src/pycamp_bot/commands/projects.py
@@ -486,7 +486,7 @@ async def show_participants(update, context):
                 text="No se indico el nombre del proyecto. Modo de uso: '/participantes <NOMBRE_PROYECTO>'"
         )
         return  
-    project_name = update.message.text.split()
+    project_name = update.message.text.split().lower()
     project_name = ' '.join(project_name[1:])
     project = Project.select().where(Project.name == project_name)
     if not project:
